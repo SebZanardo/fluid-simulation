d62375ba15663e5a49071347336290db2e25231b
Seb Zanardo <sebzanardo@optusnet.com.au>
2026-02-07 14:13:36 +1100

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fluid-simulation
+
+[Jos Stam's - Real-Time Fluid Dynamics for Games](https://graphics.cs.cmu.edu/nsp/course/15-464/Fall09/papers/StamFluidforGames.pdf)
--- src/constants.h
+++ src/constants.h
@@ -6,7 +6,7 @@
 static const u32 WINDOW_WIDTH = 528;
 static const u32 WINDOW_HEIGHT = 528;
 static const Vector2 WINDOW_SIZE = {WINDOW_WIDTH, WINDOW_HEIGHT};
-static const char WINDOW_CAPTION[] = "water-simulation";
+static const char WINDOW_CAPTION[] = "fluid-simulation";
 static const u32 WINDOW_FPS = 0;
 static const f32 FIXED_DT = 1.0f / 30.0f;
 
